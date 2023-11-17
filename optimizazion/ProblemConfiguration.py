b76106a1d7b6b77e7bb02c9e7e58984cd097a15e
mpoet11 <michaelpoetschki@uni-muenster.de>
2023-11-17 20:47:27 +0100

    Add ProblemConfiguration.py

---
+++ optimizazion/ProblemConfiguration.py
@@ -0,0 +1,16 @@
+
+
+class ProblemConfiguration:
+    def __init__(self):
+        self.watts_per_module = 800
+        self.area_per_module_in_m2 = 1.2
+        self.price_per_module_in_euro = 670
+        self.max_number_of_modules = 100
+        self.min_number_of_modules = 0
+
+        self.storage_price_per_kwh_in_euro = 1400
+        self.min_storage_size_in_kwh = 0
+        self.max_storage_size_in_kwh = 1000
+
+        self.number_of_scenarios = 100
+
---
+++ optimizazion/__init__.py
@@ -0,0 +1,1 @@
+# state is for idiots.
